40338f6b121301402b96275058cab13cf7049da9
Igor Walace Cuevas <igor.w.cuevas@gmail.com>
2021-06-04 04:03:40 -0300

    Testes de unidade - Anotações

---
+++ src/1 - Testes de Unidade/1.1 - Testes Basicos/Demo.Tests/02 - AssertStringsTests.cs
@@ -0,0 +1,90 @@
+using Xunit;
+
+namespace Demo.Tests
+{
+    public class AssertStringsTests
+    {
+        [Fact]
+        public void StringsTools_UnirNomes_RetornarNomeCompleto()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Igor", "Cuevas");
+
+            // Assert
+            Assert.Equal("Igor Cuevas", nomeCompleto);
+        }
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveIgnorarCase()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Igor", "Cuevas");
+
+            // Assert
+            Assert.Equal("IGOR CUEVAS", nomeCompleto, true);
+        }
+
+
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveConterTrecho()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Igor", "Cuevas");
+
+            // Assert
+            Assert.Contains("uevas", nomeCompleto);
+        }
+
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveComecarCom()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Igor", "Cuevas");
+
+            // Assert
+            Assert.StartsWith("Igo", nomeCompleto);
+        }
+
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveAcabarCom()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Igor", "Cuevas");
+
+            // Assert
+            Assert.EndsWith("vas", nomeCompleto);
+        }
+
+
+        [Fact]
+        public void StringsTools_UnirNomes_ValidarExpressaoRegular()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Igor", "Cuevas");
+
+            // Assert
+            Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", nomeCompleto);
+        }
+    }
+}
---
+++ src/1 - Testes de Unidade/1.1 - Testes Basicos/Demo/Calculadora.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Demo
+{
+    public class Calculadora
+    {
+        public double Somar(double numeroUm, double numeroDois) => numeroUm + numeroDois;
+        public int Dividir(int numeroUm, int numeroDois) => numeroUm / numeroDois;
+    }
+}
